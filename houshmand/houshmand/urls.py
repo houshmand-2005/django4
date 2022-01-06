4f2acfc5194bd45da4d9f8328eb4408d5c5f8b1a
houshmand <houshmand2005@gmail.com>
2022-01-06 22:07:54 +0330

    make an app and urls and views page and add name of app to setting file now i have two url 1.home(/) and 2./room

--- houshmand/houshmand/urls.py
+++ houshmand/houshmand/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path,include
+# from django.http import HttpResponse
+# def home(request):
+#     return HttpResponse('Home Page')
+# def room(request):
+#     return HttpResponse('this is room')
+        
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('',home),
+    # path('room',room),
+    path('',include('base.urls')),
 ]
