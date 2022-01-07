15284d96b6d7f7357699e208f4661a9a6abb4a1a
houshmand <houshmand2005@gmail.com>
2022-01-07 17:52:34 +0330

    idont remember changes of this files lol :)

--- houshmand/base/urls.py
+++ houshmand/base/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.home,name='home'),
-    path('room/',views.room,name='room'),
+    path('room/<str:pk>/',views.room,name='room'),
 ]
