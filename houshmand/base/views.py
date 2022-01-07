a194f8e1b9f4bdcee921d602bd420a3a6928f87b
houshmand <houshmand2005@gmail.com>
2022-01-07 17:51:32 +0330

    add context and for and better form of views

--- houshmand/base/views.py
+++ houshmand/base/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 # Create your views here.
+rooms = [
+    {'id':1,'name':'lerning python'},
+    {'id':2,'name':'lerning c++'},
+    {'id':3,'name':'lerning java'},
+]
 def home(request):
-    return HttpResponse('Home Page')
-def room(request):
-    return HttpResponse('this is room')
+    # return render(request, 'home.html',{'rooms':rooms})
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html',context)
+def room(request,pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room':room}
+    return render(request,'base/room.html',context)
