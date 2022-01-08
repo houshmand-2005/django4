2f1f3c71d18882682d9c89966b202f2176ad0fe5
houshmand <houshmand2005@gmail.com>
2022-01-08 13:43:33 +0330

    add database for rooms like id name and description

--- houshmand/base/models.py
+++ houshmand/base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class room(models.Model):
+    # host = models.CharField(max_length=)
+    # topics =
+    name = models.CharField(max_length=200)
+    deprecation = models.TextField(null=True, blank=True)
+    # participants =
+    updates = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
+        
