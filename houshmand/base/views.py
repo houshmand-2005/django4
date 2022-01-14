6df5b4844692cecd0acac94d79e5ebb5caaade68
houshmand <houshmand2005@gmail.com>
2022-01-14 19:59:42 +0330

    add Edit and delete room

--- houshmand/base/views.py
+++ houshmand/base/views.py
@@ -1,5 +1,5 @@
 import re
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
 from .models import Room
 from .forms import RoomForm
 
@@ -27,6 +27,28 @@ def room(request,pk):
     return render(request,'base/room.html',context)
 def createroom(request):
     form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
     context = {'form':form}
     return render(request, 'base/room_form.html',context)
+    
+def updateRoom(request,pk):
+    room = Room.objects.get(id=pk) 
+    form = RoomForm(instance=room)
+    context = {'form':form}
+    if request.method == 'POST':
+        form = RoomForm(request.POST,instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(request, 'base/room_form.html',context)   
+def deleteRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete_room.html',{'obj':room})
     
