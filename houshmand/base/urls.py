040cf34ee2e1929f8e116edd6a0d140e48e1764a
houshmand <houshmand2005@gmail.com>
2022-01-26 17:25:54 +0330

    add urls for logout and login and rooms mesage but idont remember what iam i did

--- houshmand/base/urls.py
+++ houshmand/base/urls.py
@@ -3,8 +3,12 @@ from . import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('login/',views.loginPage,name='login'),
+    path('register/',views.registerPage,name='register'),
+    path('logout/',views.logoutUser,name='logout'),
     path('room/<str:pk>/',views.room,name='room'),
     path('create-room/',views.createroom,name='create-room'),
     path('update-room/<str:pk>/',views.updateRoom,name='update-room'),
     path('delete-room/<str:pk>/',views.deleteRoom,name='delete-room'),
+    path('delete-message/<str:pk>/',views.deleteMessage,name='delete-message'),
+
 ]
